561b5121cfefe44c2cdd99cad3749b376e1c4315
yassir belfathi <belfathiy@gmail.com>
2025-02-24 00:38:10 +0000

    create the login page with tailwindcss and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import LoginPage from "./LoginPage/LoginPage";
+// import SignUpPage from "./SignUpPage/SignUpPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <SignUpPage /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Login" element={<LoginPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
